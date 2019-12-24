b73a801b59d944ed7d634548512783e399c7b7fa
aisudo <indigonaka@gmail.com>
2019-12-25 00:26:19 +0900

    small change

--- staff_login/staff_login.php
+++ staff_login/staff_login.php
@@ -18,11 +18,14 @@
         </div>
 
         <form class="mb-3" action="staff_login_check.php" method="post">
+        
             スタッフコード<br>
             <input type="text" name="code"><br>
             パスワード<br>
             <input type="text" name="pass"><br>
+            <small>スタッフコード:1 パスワード:1</small><br>
             <input class="btn btn-primary mt-3" type="submit" value="ログイン">
+            
         </form>
 
     </div>
