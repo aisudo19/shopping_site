3e22f9b7a78d5b4af2402c644f66f16365188a31
aisudo <indigonaka@gmail.com>
2019-12-25 02:22:46 +0900

    本番用

--- assets/db_connect.php
+++ assets/db_connect.php
@@ -1,5 +1,5 @@
 <?php 
-$dsn='mysql:dbname=tqmsbzgg_shop;host=tqmsbzgg_shop;charset=utf8';
+$dsn='mysql:dbname=tqmsbzgg_shop;host=localhost;charset=utf8';
 $user='tqmsbzgg_shop';
 $password='%RdFsbr)I})8';
 // $dsn='mysql:dbname=shop;host=localhost;charset=utf8';
