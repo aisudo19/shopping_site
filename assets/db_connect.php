a0c74961f627bd5a7f19e67256d5f926ab1f8326
aisudo <indigonaka@gmail.com>
2019-12-25 02:21:14 +0900

    本番用

--- assets/db_connect.php
+++ assets/db_connect.php
@@ -1,7 +1,10 @@
 <?php 
-$dsn='mysql:dbname=shop;host=localhost;charset=utf8';
-$user='root';
-$password='root';
+$dsn='mysql:dbname=tqmsbzgg_shop;host=tqmsbzgg_shop;charset=utf8';
+$user='tqmsbzgg_shop';
+$password='%RdFsbr)I})8';
+// $dsn='mysql:dbname=shop;host=localhost;charset=utf8';
+// $user='root';
+// $password='root';
 $dbh = new PDO($dsn, $user, $password);
 $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
