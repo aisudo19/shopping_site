b10b91477ec6aa36533ac20ddcbe78397ae4457b
aisudo <indigonaka@gmail.com>
2019-12-24 17:45:16 +0900

    db

--- shop/index.php
+++ shop/index.php
@@ -49,7 +49,7 @@
 				<?php
 
 				try {
-					$dsn='mysql:dbname=tqmsbzgg_shop;host=tqmsbzgg_shop;charset=utf8';
+					$dsn='mysql:dbname=tqmsbzgg_shop;host=localhost;charset=utf8';
 					$user='tqmsbzgg_shop';
 					$password='%RdFsbr)I})8';
 					$dbh = new PDO($dsn, $user, $password);
