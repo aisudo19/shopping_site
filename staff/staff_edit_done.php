3ba743a4f0b130e3deaf872350d8b57a3c9500ae
aisudo <indigonaka@gmail.com>
2019-12-25 00:20:17 +0900

    コミットもれ

--- staff/staff_edit_done.php
+++ staff/staff_edit_done.php
@@ -42,9 +42,9 @@
             $staff_name = $post['name'];
             $staff_pass = $post['pass'];
 
-            $dsn = 'mysql:dbname=shop;host=localhost;charset=utf8';
-            $user = 'root';
-            $password = 'root';
+            $dsn='mysql:dbname=tqmsbzgg_shop;host=localhost;charset=utf8';
+            $user='tqmsbzgg_shop';
+            $password='%RdFsbr)I})8';
             $dbh = new PDO($dsn, $user, $password);
             $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
